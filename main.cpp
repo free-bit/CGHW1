c9feef2a9bebf25039327f2622052723bcea0793
narmin9 <a.narmin20@gmail.com>
2018-10-28 16:10:30 +0300

    mirror reflectance added

--- main.cpp
+++ main.cpp
@@ -27,6 +27,11 @@ Vec3f ds_shading(const Vec3f &v, //v is used for both wi & h
   return coeff.elementwiseMultip((I*(1/(r*r)))*(pow(max((float)0, n*v),p)));
 }
 
+Vec3f reflection(const Vec3f &d, const Vec3f &n) 
+{ 
+    return d - n * (2 * (d * n)); // d - 2(d.n)n 
+} 
+
 //Ray-Triangle intersection
 //Parameters: r parameter is mutable, others are constant
 
@@ -69,6 +74,134 @@ void intersection_test(ray &r,
   }
 }
 
+Vec3f raycolor(ray &r,
+               vector<Mesh> &meshes,
+               vector<Triangle> &triangles,
+               vector<Sphere> &spheres,
+               Vec3f &ambient_light,
+               vector<PointLight> &point_lights,
+               vector<Material> &materials,
+               Vec3i &background_color,
+               int max_recursion_depth,
+               float shadow_ray_epsilon,
+               const unsigned int &depth = 0)
+{
+  if (depth > max_recursion_depth)
+  {
+    Vec3f bg_float(background_color.r, background_color.g, background_color.b);
+    return bg_float;
+  }
+
+  //intersection test for all objects in the scene
+  intersection_test(r,
+                    meshes,
+                    triangles,
+                    spheres,
+                    shadow_ray_epsilon);
+
+  Vec3f accumulator;
+  if(r.t<numeric_limits<float>::infinity())
+  {
+    Material material;
+    int material_id;
+
+    Vec3f point_of_intersection(r.e + r.d*r.t);
+    Vec3f normal;
+
+    switch(r.c)
+    {
+      case 's':
+      {
+        Sphere* ptr = ((Sphere *) r.obj);
+        material_id = ptr->material_id;
+        normal = (point_of_intersection-ptr->center)*(1/ptr->radius); //Normalized
+        break;
+      }
+      case 'm':
+      {
+        Mesh* ptr = ((Mesh *) r.obj);
+        material_id = ptr->material_id;
+        normal = ptr->faces[0].normal;
+        break;
+      }
+      case 't':
+      {
+        Triangle* ptr = ((Triangle *) r.obj);
+        material_id = ptr->material_id;
+        normal = ptr->indices.normal;
+        break;
+      }
+      default:
+        cout<<"error"<<endl;
+    }
+
+    material_id--; //zero-indexed
+
+    material.ambient = materials[material_id].ambient;  //ambient coeff
+    material.diffuse = materials[material_id].diffuse;  //diffuse coeff
+    material.specular = materials[material_id].specular;//specular coeff
+    material.mirror = materials[material_id].mirror;    //mirror coeff
+    material.phong_exponent = materials[material_id].phong_exponent;
+
+    //Ambient component calculation
+    accumulator = accumulator + ambient_light.elementwiseMultip(material.ambient);
+
+    for(auto &light : point_lights)
+    {
+      ray shadowRay(point_of_intersection,
+                    light.position-point_of_intersection);
+      float distance2light = shadowRay.d.length();        //float distance to point light
+      Vec3f vector2light(shadowRay.d*(1/distance2light)); //Normalized vector
+
+      //intersection test for all objects in the scene
+      intersection_test(shadowRay,
+                        meshes,
+                        triangles,
+                        spheres,
+                        shadow_ray_epsilon);
+      //object is in shadow
+      // cout<<"Shadow t:"<<shadowRay.t<<endl;
+      // cout<<"Distance2Light:"<<distance2light<<endl;
+      if(shadowRay.t<1)
+      {
+        continue;
+      }
+      //object is not in shadow, calculate diffuse component
+      Vec3f diffuse_component = ds_shading(vector2light,
+                                           normal,
+                                           material.diffuse,
+                                           light.intensity,
+                                           distance2light);
+      Vec3f specular_component = ds_shading((vector2light+(-r.d)).normalize(),
+                                            normal,
+                                            material.specular,
+                                            light.intensity,
+                                            distance2light,
+                                            material.phong_exponent);
+      accumulator = accumulator+diffuse_component+specular_component;
+      if (material.mirror.x || material.mirror.y || material.mirror.z) {
+        Vec3f R = reflection(r.d, normal); 
+        ray reflected_ray(point_of_intersection + R * shadow_ray_epsilon, R);
+        accumulator = accumulator +  material.mirror.elementwiseMultip(raycolor(reflected_ray,
+                                            meshes,
+                                            triangles,
+                                            spheres,
+                                            ambient_light,
+                                            point_lights,
+                                            materials,
+                                            background_color,
+                                            max_recursion_depth,
+                                            shadow_ray_epsilon,
+                                            depth + 1
+                                            ));
+ 
+      }
+    }
+  }
+  return accumulator;
+  
+}
+
 int main(int argc, char* argv[])
 {
     //Testing
@@ -89,175 +222,59 @@ int main(int argc, char* argv[])
 
     scene.loadFromXml(argv[1]);
 
-    // The code below creates a test pattern and writes
-    // it to a PPM file to demonstrate the usage of the
-    // ppm_write function.
-
-    /* Remove later
-    const RGB BAR_COLOR[8] =
-    {
-        { 255, 255, 255 },  // 100% White
-        { 255, 255,   0 },  // Yellow
-        {   0, 255, 255 },  // Cyan
-        {   0, 255,   0 },  // Green
-        { 255,   0, 255 },  // Magenta
-        { 255,   0,   0 },  // Red
-        {   0,   0, 255 },  // Blue
-        {   0,   0,   0 },  // Black
-    };
-
-    int width = 640, height = 480;
-    int columnWidth = width / 8;
-
-    unsigned char* image = new unsigned char [width * height * 3];
-
-    int i = 0;
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            int colIdx = x / columnWidth;
-            image[i++] = BAR_COLOR[colIdx][0];
-            image[i++] = BAR_COLOR[colIdx][1];
-            image[i++] = BAR_COLOR[colIdx][2];
-        }
-    } Remove later*/
-
-    //MAIN
     for(auto &camera : scene.cameras)
     {
         string img_name = camera.image_name;
+
         int img_width = camera.image_width,
             img_height = camera.image_height;
         Vec3f u_axis = camera.up.crossProduct(-camera.gaze);
+
         float horizontal = (camera.near_plane.r-camera.near_plane.l)/img_width,
               vertical = (camera.near_plane.t-camera.near_plane.b)/img_height;
 
         unsigned char* image = new unsigned char [img_width * img_height * 3];
 
         int pixel_channel = 0;
-        // int count_true=0,count_false=0;
+
         float shadow_ray_epsilon = scene.shadow_ray_epsilon;
+
         ray r;
         r.c = '\0';
         r.obj = NULL;
         r.e = camera.position; //eyepoint
+
         float v = camera.near_plane.b+vertical*0.5;
+
         for (int y = 0; y < img_height; y++)
         {
           float u = camera.near_plane.l+horizontal*0.5;
             for (int x = 0; x < img_width; x++)
             {
-                image[pixel_channel]=0;
-                image[pixel_channel+1]=0;
-                image[pixel_channel+2]=0;
                 //r.obj = NULL;
                 r.t = numeric_limits<float>::infinity();
                 r.d = camera.gaze*camera.near_distance + u_axis*u - camera.up*v;
-                //intersection test for all objects in the scene
-                intersection_test(r,
-                                  scene.meshes,
-                                  scene.triangles,
-                                  scene.spheres,
-                                  shadow_ray_epsilon);
 
-                if(r.t<numeric_limits<float>::infinity())
-                {
-                  Material material;
-                  int material_id;
+                Vec3f color = raycolor(r,
+                                       scene.meshes,
+                                       scene.triangles,
+                                       scene.spheres,
+                                       scene.ambient_light,
+                                       scene.point_lights,
+                                       scene.materials,
+                                       scene.background_color,
+                                       scene.max_recursion_depth,
+                                       shadow_ray_epsilon);
 
-                  Vec3f point_of_intersection(r.e + r.d*r.t);
-                  Vec3f normal;
+                image[pixel_channel++]=color.x > 255 ? 255 : color.x;
+                image[pixel_channel++]=color.y > 255 ? 255 : color.y;
+                image[pixel_channel++]=color.z > 255 ? 255 : color.z;
 
-                  switch(r.c)
-                  {
-                    case 's':
-                    {
-                      Sphere* ptr = ((Sphere *) r.obj);
-                      material_id = ptr->material_id;
-                      normal = (point_of_intersection-ptr->center)*(1/ptr->radius); //Normalized
-                      break;
-                    }
-                    case 'm':
-                    {
-                      Mesh* ptr = ((Mesh *) r.obj);
-                      material_id = ptr->material_id;
-                      normal = ptr->faces[0].normal;
-                      break;
-                    }
-                    case 't':
-                    {
-                      Triangle* ptr = ((Triangle *) r.obj);
-                      material_id = ptr->material_id;
-                      normal = ptr->indices.normal;
-                      break;
-                    }
-                    default:
-                      cout<<"error"<<endl;
-                  }
-                  material_id--; //zero-indexed
-                  material.ambient = scene.materials[material_id].ambient;  //ambient coeff
-                  material.diffuse = scene.materials[material_id].diffuse;  //diffuse coeff
-                  material.specular = scene.materials[material_id].specular;//specular coeff
-                  material.mirror = scene.materials[material_id].mirror;    //mirror coeff
-                  material.phong_exponent = scene.materials[material_id].phong_exponent;
-
-                  //Ambient component calculation
-                  Vec3f accumulator(scene.ambient_light.elementwiseMultip(material.ambient));
-                  // TODO: might require clamping
-
-                  for(auto &light : scene.point_lights)
-                  {
-                    ray shadowRay(point_of_intersection,
-                                  light.position-point_of_intersection);
-                    float distance2light = shadowRay.d.length();        //float distance to point light
-                    Vec3f vector2light(shadowRay.d*(1/distance2light)); //Normalized vector
-
-                    //intersection test for all objects in the scene
-                    intersection_test(shadowRay,
-                                      scene.meshes,
-                                      scene.triangles,
-                                      scene.spheres,
-                                      shadow_ray_epsilon);
-                    //object is in shadow
-                    // cout<<"Shadow t:"<<shadowRay.t<<endl;
-                    // cout<<"Distance2Light:"<<distance2light<<endl;
-                    if(shadowRay.t<1)
-                    {
-                      continue;
-                    }
-                    //object is not in shadow, calculate diffuse component
-                    Vec3f diffuse_component = ds_shading(vector2light,
-                                                         normal,
-                                                         material.diffuse,
-                                                         light.intensity,
-                                                         distance2light);
-                    Vec3f specular_component = ds_shading((vector2light+(-r.d)).normalize(),
-                                                          normal,
-                                                          material.specular,
-                                                          light.intensity,
-                                                          distance2light,
-                                                          material.phong_exponent);
-                    accumulator=accumulator+diffuse_component+specular_component;
-                  }
-                  image[pixel_channel]=image[pixel_channel]+accumulator.x > 255 ? 255 : image[pixel_channel]+accumulator.x;
-                  image[pixel_channel+1]=image[pixel_channel+1]+accumulator.y > 255 ? 255 : image[pixel_channel+1]+accumulator.y;
-                  image[pixel_channel+2]=image[pixel_channel+2]+accumulator.z > 255 ? 255 : image[pixel_channel+2]+accumulator.z;
-
-                  //...
-                  pixel_channel+=3;
-                }
-                else
-                {
-                  //TODO: might require clamping
-                  image[pixel_channel++]=scene.background_color.r;
-                  image[pixel_channel++]=scene.background_color.g;
-                  image[pixel_channel++]=scene.background_color.b;
-                }
                 u += horizontal;
             }
             v += vertical;
         }
+
         write_ppm(img_name.c_str(), image, img_width, img_height);
         delete [] image;
     }
